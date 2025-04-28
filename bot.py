6eb476c1a19bf83de75a3fe14c1b548677a9b7e8
joaopantoja <joaopantojacomercial@gmail.com>
2025-04-28 01:36:44 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,7 @@
 from telegram.ext import ApplicationBuilder, CommandHandler
 import random
 
-TOKEN = "7934330924:AAF6xHyuus2I5lJcjKgUFK7mlISUoxXIQJg
-"
+TOKEN = "7934330924:AAF6xHyuus2I5lJcjKgUFK7mlISUoxXIQJg"
 
 # Cria o bot
 app = ApplicationBuilder().token(TOKEN).build()
